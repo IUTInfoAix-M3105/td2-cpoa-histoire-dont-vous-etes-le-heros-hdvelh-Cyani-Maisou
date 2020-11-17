50c688cfd43c0a9b716a43f2061d08a94d196ad6
Cyani-Maisou <tsuki.cookies@gmail.com>
2020-11-17 16:35:13 +0100

    modifications pour correspondre a la correction, methode run() fonctionnelle

--- src/pracHDVELH/NodeMultiple.java
+++ src/pracHDVELH/NodeMultiple.java
@@ -34,25 +34,25 @@ public class NodeMultiple {
 //	public NodeMultiple getDaughter(int i) {
 //		/* TO BE COMPLETED */
 //	}
-//
-//	/**
-//	 * Sets the {@code i}th daughter node to the input parameter {@code daughter}.
-//	 * Should be used cautiously, since {@code i} may not be the first index
-//	 * available (i.e. there may be lower indexes which do not refer to any
-//	 * daughter).
-//	 *
-//	 * If a daughter node is already referred to at this index then it is erased
-//	 * with {@code daughter}.
-//	 *
-//	 * Aborts if the index {@code i} is out of range.
-//	 *
-//	 * @param daughter the node to be linked as a daughter of {@code this} node.
-//	 * @param i        the daughter node's index
-//	 */
-//	public void setDaughter(NodeMultiple daughter, int i) {
-//		/* TO BE COMPLETED */
-//	}
-//
+
+	/**
+	 * Sets the {@code i}th daughter node to the input parameter {@code daughter}.
+	 * Should be used cautiously, since {@code i} may not be the first index
+	 * available (i.e. there may be lower indexes which do not refer to any
+	 * daughter).
+	 *
+	 * If a daughter node is already referred to at this index then it is erased
+	 * with {@code daughter}.
+	 *
+	 * Aborts if the index {@code i} is out of range.
+	 *
+	 * @param daughter the node to be linked as a daughter of {@code this} node.
+	 * @param i        the daughter node's index
+	 */
+	public void setDaughter(NodeMultiple daughter, int i) {
+		/* TO BE COMPLETED */
+	}
+
 //	/**
 //	 * @return all the daughters
 //	 */
@@ -66,19 +66,19 @@ public class NodeMultiple {
 //	public void setDaughters(NodeMultiple[] daughters) {
 //		/* TO BE COMPLETED */
 //	}
-//
-//	/**
-//	 * Adds the given {@code daughter} node at the first available index.
-//	 *
-//	 * If the max number of daughters ({@link #NODE_MAX_ARITY}) is already reached
-//	 * nothing happens (no abort).
-//	 *
-//	 * @param daughter
-//	 */
-//	public void addDaughter(NodeMultiple daughter) {
-//		/* TO BE COMPLETED */
-//	}
-//
+
+	/**
+	 * Adds the given {@code daughter} node at the first available index.
+	 *
+	 * If the max number of daughters ({@link #NODE_MAX_ARITY}) is already reached
+	 * nothing happens (no abort).
+	 *
+	 * @param daughter
+	 */
+	public void addDaughter(NodeMultiple daughter) {
+		/* TO BE COMPLETED */
+	}
+
 //	/**
 //	 * @return the content data
 //	 */
